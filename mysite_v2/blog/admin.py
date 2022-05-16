0e67abf0724341e7da36573fb50d5b319ef68402
Bartosz Woźniak <101841466+Lejonke@users.noreply.github.com>
2022-05-16 18:22:24 +0200

    Add files via upload

--- mysite_v2/blog/admin.py
+++ mysite_v2/blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Book, File_format, About, Author
+from .models import Book, FileFormat, About, Author
 
 admin.site.register(Book)
 
--- mysite_v2/blog/models.py
+++ mysite_v2/blog/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 
 class Author(models.Model):
+    """ Klasa Autora, zawiera imię i nazwisko autora """
+    
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
 
@@ -12,34 +15,68 @@ class Author(models.Model):
 
 
 class About(models.Model):
+    """ Klasa Tematyki, zawiera pole tekstowe do 50 znaków, można w przyszłości rozbudować o dodatkowe informacje
+        (np. 3 ramki choices zawierające szczegółowe działy IT, streszczenia texfield,
+        spis treści)"""
+    
     about = models.CharField(max_length = 50)
 
     def __str__(self):
         return self.about
 
 
-class File_format(models.Model):
-    
-    class book_format(models.TextChoices):
-        #FRESHMAN = 'FR', _('Freshman')
-        Hard_Cover_Printed = 'HCP', (' Druk/papier w twardej okładce ')
-        Soft_Cover_Printed = 'PS', (' Druk/papier w miękkiej okładce') 
-        EPUB = 'epub' ,('e-publikacja')
-        PDF = 'pdf', ('pdf jaki jest każdy widzi')
+class FileFormat(models.Model):
+     """ Klasa Formatu Pliku - Ksiązki..."""
+     
+#       condition = models.TextField('''max_length = 50''')
+     
+    class BookFormat(models.TextChoices):
+        """ Klasa Formatu Ksiązki, wewnętrzna w FileFormat czy fizyczna (okładka twarda/miękka) czy plik, oraz stan ksiązki ("nówka",
+             "używana" w przypadku fizycznych, czy np. skan w przypadku starych niedostepnych już
+              książek dostępnych w formie wirtualnej """
         
-    condition = models.TextField(max_length = 100)
-
+        hard_cover_printed = 'HCP', _(' Druk/papier w twardej okładce ')
+        soft_cover_printed = 'PS', _(' Druk/papier w miękkiej okładce') 
+        epub = 'epub' , _('e-publikacja')
+        pdf = 'pdf', _('pdf jaki jest każdy widzi')
+        
+    book_format = models.CharField(
+        max_length = 20,
+        choices = BookFormat.choices,
+        default = BookFormat.epub)
+        
     def __str__(self):
         return self.file_format
-
-
+    
+#class Student(models.Model):
+#
+#    class YearInSchool(models.TextChoices):
+#        FRESHMAN = 'FR', _('Freshman')
+#        SOPHOMORE = 'SO', _('Sophomore')
+#        JUNIOR = 'JR', _('Junior')
+#        SENIOR = 'SR', _('Senior')
+#        GRADUATE = 'GR', _('Graduate')
+#
+#    year_in_school = models.CharField(
+#        max_length=2,
+#        choices=YearInSchool.choices,
+#        default=YearInSchool.FRESHMAN,
+#    )
+        
+#    def is_upperclass(self):
+  #          return self.year_in_school in {             #tutorial z docs.djangoproject.com
+    #            self.YearInSchool.JUNIOR,
+      #          self.YearInSchool.SENIOR,
+        #    }
 
 class Book(models.Model):
+    """ Klasa Ksiązki,
+        klucz obcy do klas zdefiniowanych powyżej """
 
-    Hard_Cover_Printed = 'HCP'
-    Soft_Cover_Printed = 'PS'
-    EPUB = 'epub' 
-    PDF = 'pdf'
+    hard_cover_printed = 'HCP'
+    soft_cover_printed = 'PS'
+    epub = 'epub' 
+    pdf = 'pdf'
 
     author = models.ForeignKey(Author, on_delete = models.CASCADE)
     
@@ -50,8 +87,7 @@ class Book(models.Model):
     published_date = models.DateTimeField(
         blank = True, null = True)
 
-    
-    book_format= models.ForeignKey(File_format, on_delete = models.CASCADE)
+    book_format= models.ForeignKey(FileFormat, on_delete = models.CASCADE)
 
     def info(self):
         self.published_date = timezone()
